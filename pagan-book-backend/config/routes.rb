cffaf8e9996ea8bc6c3068f1b1c2e2ed9e048ee1
creinken <reinkendigital@gmail.com>
2022-08-02 17:09:37 -0500

    configured groups and posts routes

--- pagan-book-backend/config/routes.rb
+++ pagan-book-backend/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/create'
-  get 'posts/destroy'
-  get 'groups/index'
-  get 'groups/show'
-  get 'groups/create'
-  get 'groups/destroy'
+  resources :groups do
+      resources :posts
+  end
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
